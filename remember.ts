dc8821791d33436f74274c76ef17e51fcfa302a6
musolffc <musolffc@gmail.com>
2017-10-24 04:53:53 +0000

    Update remember.ts

--- remember.ts
+++ remember.ts
@@ -4,9 +4,3 @@ function remember(name){
     const here = magik.hic();
     magik.memento.setItem(name, here);
 }
-
-
-function recall(name){
-    const memory = magik.memento.getItem(name);
-    magik.dixit(memory);
-}
